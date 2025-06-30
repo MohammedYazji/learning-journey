560e2b56dc088feb779f169b1f74c2005bacce83
MohammedYazji <mahmadyazji@gmail.com>
2025-06-30 15:59:53 +0300

    from callbacks to promises

--- tutorial_projects/nodejs/3-asynchronous-JS/index.js
+++ tutorial_projects/nodejs/3-asynchronous-JS/index.js
@@ -5,16 +5,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
   console.log(`Breed: ${data}`);
 
   //   making the http request
+  //   get returns a promise so we can handle this promise
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      // handle error and stop
-      if (err) return console.log(err.message);
-
+    .then((res) => {
       console.log(res.body.message);
 
       fs.writeFile(`dog-img.txt`, res.body.message, (err) =>
         console.log('Random dog image saved to file!')
       );
-    });
+    })
+    .catch((err) => console.log(err.message));
 });
