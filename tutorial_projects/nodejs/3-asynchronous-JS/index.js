3af0a7390bca296f28c2eaf98c20f752812b790f
MohammedYazji <mahmadyazji@gmail.com>
2025-06-30 16:28:46 +0300

    building promises

--- tutorial_projects/nodejs/3-asynchronous-JS/index.js
+++ tutorial_projects/nodejs/3-asynchronous-JS/index.js
@@ -1,19 +1,45 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+// build a promises
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      // the result of the promise
 
-  //   making the http request
-  //   get returns a promise so we can handle this promise
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
+      // the error will access from catch method
+      if (err) reject('I could not find that file 🥲');
+      // as argument in then method [success]
+      //   we can call it anything in then callback argument [maybe result]
+      resolve(data);
+    });
+  });
+};
 
-      fs.writeFile(`dog-img.txt`, res.body.message, (err) =>
-        console.log('Random dog image saved to file!')
-      );
-    })
-    .catch((err) => console.log(err.message));
-});
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('Could not write file 🥲');
+      resolve('success');
+    });
+  });
+};
+
+// chaining functions return promises
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+
+    // get returns a promise
+    // so we can use then catch in all this function call
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro(`dog-img.txt`, res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image saved to file!');
+  })
+  .catch((err) => console.log(err));
