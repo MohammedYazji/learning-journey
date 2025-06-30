e91b576f69f584d3d8c70951023e4d4c968b87c6
MohammedYazji <mahmadyazji@gmail.com>
2025-06-30 19:02:35 +0300

    returning values from async functions

--- tutorial_projects/nodejs/3-asynchronous-JS/index.js
+++ tutorial_projects/nodejs/3-asynchronous-JS/index.js
@@ -40,27 +40,41 @@ const getDogPic = async () => {
     console.log('Random dog image saved to file!');
   } catch (err) {
     console.log(err);
+    // we must throw error so this will return rejected after the execution done if there is error
+    throw err;
   }
+  return '2: READY 🐶';
 };
-getDogPic();
-
+/////////////
+// runs in the background because takes some times
+// and returns a promise so x now is promise because the function is still running
+// x is not ready yet while the function running
 /*
-// chaining functions return promises
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
-    console.log(`Breed: ${data}`);
-
-    // get returns a promise
-    // so we can use then catch in all this function call
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
-
-    return writeFilePro(`dog-img.txt`, res.body.message);
-  })
-  .then(() => {
-    console.log('Random dog image saved to file!');
-  })
-  .catch((err) => console.log(err));
+const x = getDogPic();
+console.log(x)
 */
+// so we need to await this promise until finish execution,
+// 1: by then,
+/*
+console.log('1: will get dog pics!');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  })
+  .catch((err) => console.log('Error 💥'));
+  */
+
+// 2: or by async/await [best solution]
+
+// we can make it as expression without call it later😅
+(async () => {
+  try {
+    console.log('1: will get dog pics!');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Done getting dog pics!');
+  } catch (err) {
+    console.log('Error 💥');
+  }
+})();
