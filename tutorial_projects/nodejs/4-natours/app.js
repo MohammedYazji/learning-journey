2f3a722cc8b8a8fcbbf5a6c223eb9219e8acb543
MohammedYazji <mahmadyazji@gmail.com>
2025-07-03 12:44:06 +0300

    Environment Variables

--- tutorial_projects/nodejs/4-natours/app.js
+++ tutorial_projects/nodejs/4-natours/app.js
@@ -9,8 +9,11 @@ const app = express();
 // 1. Middlewares
 // general middlewares for all requests and routes
 
-// use 3rd Party [logging to console the req information]
-app.use(morgan('dev'));
+// only use morgan in the development environment
+if (process.env.NODE_ENV === 'development') {
+  // use 3rd Party [logging to console the req information]
+  app.use(morgan('dev'));
+}
 
 // express.json is a middleware: function can modify the incoming request data
 // we call it middleware because its between the req and the res
@@ -18,7 +21,7 @@ app.use(express.json());
 
 // serving static files [just for knowing don't use it]
 // here we serve public folder
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
 
 // create our middleware
 // app.use => add the middleware to the middleware stack
--- tutorial_projects/nodejs/4-natours/server.js
+++ tutorial_projects/nodejs/4-natours/server.js
@@ -1,7 +1,17 @@
+const dotenv = require('dotenv');
+// we use config to declare where the config file that include all variables is
+// so this will read the variable from the file, then save them into nodejs environment variables
+dotenv.config({ path: './config.env' });
+
+// must require it after set the config not before, so in app.js when i use environment variables will be declared before
 const app = require('./app');
 
-// db config, error handling , environment variable all will be here
-const port = 3000;
+// to see all the environment variables
+// console.log(process.env);
+// to know in which environment we use now
+// console.log(app.get('env'));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
