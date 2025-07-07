4c1c8da71e369262ed46bee5ebd8dddac96383d9
MohammedYazji <mahmadyazji@gmail.com>
2025-07-07 16:20:22 +0300

    Reading Documents

--- tutorial_projects/nodejs/4-natours/controllers/tourController.js
+++ tutorial_projects/nodejs/4-natours/controllers/tourController.js
@@ -1,13 +1,20 @@
 const Tour = require('./../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    //   results: tours.length,
-    //   data: { tours },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: { tours },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 exports.createTour = async (req, res) => {
   try {
@@ -33,19 +40,23 @@ exports.createTour = async (req, res) => {
     });
   }
 };
-exports.getTour = (req, res) => {
-  // console.log(req.params);
-  // type coercion 🩷 [must convert from string to a number]
-  const id = req.params.id * 1;
+exports.getTour = async (req, res) => {
+  try {
+    // Tour.findOne({_id: req.params.id}) // IN MONGODB
+    const tour = await Tour.findById(req.params.id);
 
-  // const tour = tours.find((el) => el.id === id);
-
-  // res.status(200).json({
-  //   status: 'success',
-  //   data: {
-  //     tour,
-  //   },
-  // });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 exports.updateTour = (req, res) => {
   res.status(200).json({
