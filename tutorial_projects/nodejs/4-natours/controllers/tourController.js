a2dc8269f0c1c4e614dd64620a0f923c2e26e161
MohammedYazji <mahmadyazji@gmail.com>
2025-07-10 05:11:51 +0300

    Making the API Better Aliasing

--- tutorial_projects/nodejs/4-natours/controllers/tourController.js
+++ tutorial_projects/nodejs/4-natours/controllers/tourController.js
@@ -1,5 +1,15 @@
 const Tour = require('../models/tourModel');
 
+// middleware for '/top-5-cheap'
+// 5 best and cheapest tours
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
+  next(); // to implement getAllTours after this
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
--- tutorial_projects/nodejs/4-natours/routes/tourRoutes.js
+++ tutorial_projects/nodejs/4-natours/routes/tourRoutes.js
@@ -4,6 +4,12 @@ const tourController = require('./../controllers/tourController');
 // this router is kind of sub-application for tours resource
 const router = express.Router();
 
+// so we need to make a middleware just for this unique route to manipulate req before move to getAllTours
+// 127.0.0.1:3000/api/v1/tours?limit=5&sort=-ratingsAverage,price
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router
   .route('/')
   .get(tourController.getAllTours)
