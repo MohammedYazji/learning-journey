2c7e66cc1fb848008c77e9b5ce133cbcd0927dbd
MohammedYazji <mahmadyazji@gmail.com>
2025-07-10 20:04:28 +0300

    values and variables in JS

---
+++ tutorial_projects/vanilla-JS/01- Fundamentals-Part-1/script.js
@@ -0,0 +1,38 @@
+////////////////////////////////////
+// LINKING a JAVASCRIPT FILE //
+/*
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(40 + 10 - 12);
+*/
+////////////////////////////////////
+// VALUES AND VARIABLES //
+
+console.log("Mohammed");
+
+// Store the data inside a box then access it everywhere
+// We Use CamelCase notation to Declare a variable in JS, while in Python we use snake_case
+let firstName = "Mohammed";
+console.log(firstName, firstName);
+
+// Don't Do This?!?
+// let 2years = 600 // Syntax Error
+// let mohammed&yazji = 'Mohammed Yazji' // Syntax Error
+// let new = 27 // Syntax Error [reserved Word]
+
+// You Can Do This..
+let $function = 27;
+
+// To Declare a constant
+const PI = 3.14;
+
+// Use Meaningful names: 👍
+let myFirstJob = "Programmer";
+let myCurrentJob = "Dentist";
+// Instead of this.. ❌
+// let job1 = "Programmer";
+// let job2 = "Dentist";
+
+////////////////////////////////////
+//  //
