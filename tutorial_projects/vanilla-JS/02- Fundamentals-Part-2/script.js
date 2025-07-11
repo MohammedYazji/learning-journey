f236f2e11babd203ad69bdcc4b586af2938a6caa
MohammedYazji <mahmadyazji@gmail.com>
2025-07-11 08:53:37 +0300

    Function Declarations vs. Expressions

--- tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
+++ tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
@@ -17,7 +17,7 @@ const passTest = true;
 */
 ///////////////////////////////////////
 // Functions //
-
+/*
 // piece of code we can reuse it over and over again
 // declaring function [DRY]
 function logger() {
@@ -44,3 +44,24 @@ console.log(appleOrangeJuice);
 
 // number also is a built-in function, console.log also, and more...
 const num = Number("15");
+*/
+///////////////////////////////////////
+// Function Declarations vs. Expressions //
+
+// Function declaration
+
+// we can access the function declaration before initialization [Hoisting]
+const age1 = calcAge1(2003);
+
+function calcAge1(birthYeah) {
+  return 2025 - birthYeah;
+}
+
+// Function expression
+// [we assign the function as a value to the variable calcAge2] => [expressions produce value]
+const calcAge2 = function (birthYeah) {
+  return 2025 - birthYeah;
+};
+const age2 = calcAge2(2003);
+
+console.log(age1, age2);
