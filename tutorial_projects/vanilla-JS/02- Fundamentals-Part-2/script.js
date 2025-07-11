e1fecbd920bf6971d4720d77ca1a19c7d6d29130
MohammedYazji <mahmadyazji@gmail.com>
2025-07-11 16:03:52 +0300

    Introduction to Object

--- tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
+++ tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
@@ -281,7 +281,7 @@ HINT: Remember that an array needs a value in each position, and that value can 
 
 GOOD LUCK 😀
 */
-
+/*
 // const calcTip = function (bill) {
 //   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 // };
@@ -298,3 +298,26 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
+*/
+///////////////////////////////////////
+// Introduction to Object //
+
+const myArray = [
+  "Mohammed",
+  "Yazji",
+  2025 - 2003,
+  "Engineer",
+  ["Ali", "Ahmad", "Abood"],
+];
+
+// key-value pairs inside curly brackets
+// can include any data types such as the arrays
+// this way to create an object called =>  object literal syntax, because literally we use { } to declare it
+// and here the order doesn't matter
+const myObj = {
+  firstName: "Mohammed",
+  lastName: "Yazji",
+  age: 2025 - 2003,
+  job: "Engineer",
+  friends: ["Ali", "Ahmad", "Abood"],
+};
