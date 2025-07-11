51691ba6de72aead7585d78f08beffeab57a57d8
MohammedYazji <mahmadyazji@gmail.com>
2025-07-11 08:47:41 +0300

    Functions

--- tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
+++ tutorial_projects/vanilla-JS/02- Fundamentals-Part-2/script.js
@@ -2,7 +2,7 @@
 
 ///////////////////////////////////////
 // Activating Strict Mode //
-
+/*
 // to avoid pugs in our code
 
 let hasDriversLicense = false;
@@ -14,3 +14,33 @@ const passTest = true;
 // also if i defined reserved names ❌
 // const interface = "Audio"; // SyntaxError: Unexpected strict mode reserved word
 // const if = 534; // SyntaxError: Unexpected token 'if'
+*/
+///////////////////////////////////////
+// Functions //
+
+// piece of code we can reuse it over and over again
+// declaring function [DRY]
+function logger() {
+  // block of instructions
+  console.log("My name is Mohammed");
+}
+
+// Calling / Running / Invoking function
+logger();
+logger();
+logger();
+
+// we can pass data to the function, or return data from it
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// number also is a built-in function, console.log also, and more...
+const num = Number("15");
