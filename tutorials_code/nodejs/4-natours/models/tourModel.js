bb72faeb25cb217d74293c22604027bbfb1278c2
MohammedYazji <mahmadyazji@gmail.com>
2025-07-17 09:16:40 +0300

    Document Middleware

--- tutorials_code/nodejs/4-natours/models/tourModel.js
+++ tutorials_code/nodejs/4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 // creating a schema for tours
 const tourSchema = new mongoose.Schema(
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -73,6 +75,29 @@ const tourSchema = new mongoose.Schema(
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
+
+///////
+// DOCUMENT MIDDLEWARE
+// define a middleware on the tour schema that will execute before certain event in this case[ the save event .save() and .create()]
+tourSchema.pre('save', function (next) {
+  // will print the document is being process
+  // console.log(this);
+  // add new property for this called slug and put inside it the name and store the slug with the document in the DB
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+tourSchema.pre('save', function (next) {
+  console.log('Will save document...');
+  next();
+});
+
+// middleware after the process of saving
+// receive doc argument which the document which we have saved
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next(); // right there's no next after this middleware in thestack but it's best practice to put it
+});
+///////
 
 // creating a model from this schema
 const Tour = mongoose.model('Tour', tourSchema);
