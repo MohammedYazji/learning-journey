dddc640c66c5d241ca05ae552ff6e0907c015ad4
MohammedYazji <mahmadyazji@gmail.com>
2025-07-21 19:12:41 +0300

    Handling Invalid Database IDs

--- tutorials_code/nodejs/4-natours/controllers/errorController.js
+++ tutorials_code/nodejs/4-natours/controllers/errorController.js
@@ -1,3 +1,10 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -39,6 +46,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // its not best practice to mutate the original error object so we copy it before
+    let error = { ...err };
+
+    if (error.name === 'CastError') {
+      error = handleCastErrorDB(error); // this function will return AppError
+    }
+
+    sendErrorProd(error, res);
   }
 };
