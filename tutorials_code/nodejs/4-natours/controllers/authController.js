7db4ccf5325c654a48a77a42bfebc85557a7ece4
MohammedYazji <mahmadyazji@gmail.com>
2025-07-23 16:32:46 +0300

    Signing up Users

--- tutorials_code/nodejs/4-natours/controllers/authController.js
+++ tutorials_code/nodejs/4-natours/controllers/authController.js
@@ -1,12 +1,28 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // just specify the data we need from the request
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // create our token
+  // valid until 90 days
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  // send the token to the client
 
   //   then send the new user to the client
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
