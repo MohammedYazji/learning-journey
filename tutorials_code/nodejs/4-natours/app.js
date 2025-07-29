80fa735cfd35f48f7da7b5111ddaf94a6d8c5a8b
MohammedYazji <mahmadyazji@gmail.com>
2025-07-29 15:00:16 +0300

    Setting Security HTTP Headers

--- tutorials_code/nodejs/4-natours/app.js
+++ tutorials_code/nodejs/4-natours/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,12 +14,17 @@ const app = express();
 // 1. GLOBAL Middlewares
 // general middlewares for all requests and routes
 
+// SET SECURITY HTTP HEADERS //
+app.use(helmet());
+
+// DEVELOPMENT LOGGING //
 // only use morgan in the development environment
 if (process.env.NODE_ENV === 'development') {
   // use 3rd Party [logging to console the req information]
   app.use(morgan('dev'));
 }
 
+// LIMIT REQUEST FROM SAME API //
 // we need to secure our api by limit the rating of request of the same ip address
 const limiter = rateLimit({
   max: 100, // number of requests
@@ -28,18 +34,20 @@ const limiter = rateLimit({
 // we will implement this middleware for all routes have /api
 app.use('/api', limiter);
 
+// BODY PARSER, READING DATA FROM BODY INTO req.body //
 // express.json is a middleware: function can modify the incoming request data
 // we call it middleware because its between the req and the res
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 
-// serving static files [just for knowing don't use it]
+// serving static files [just for knowing don't use it] //
 // here we serve public folder
 // app.use(express.static(`${__dirname}/public`));
 
+// TEST MIDDLEWARE //
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // each request has headers
-  console.log(req.headers);
+  // console.log(req.headers);
   next();
 });
 
