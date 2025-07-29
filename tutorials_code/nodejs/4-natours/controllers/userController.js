ae8ab5c5d491ae6c4e1b5a4b2416723c149e8019
MohammedYazji <mahmadyazji@gmail.com>
2025-07-29 13:47:08 +0300

    Updating the Current User: Data

--- tutorials_code/nodejs/4-natours/controllers/userController.js
+++ tutorials_code/nodejs/4-natours/controllers/userController.js
@@ -1,5 +1,18 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) {
+      newObj[el] = obj[el];
+    }
+  });
+
+  // just push name and email in the object if exist nothing else
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -9,6 +22,34 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) if user POST password data create error
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This rout is not for password updates, Please use /updateMyPassword.',
+      ),
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  // we need to be sure that just the user can implement the email and password
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
