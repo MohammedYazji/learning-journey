9102d179fb413d9d5cfe9bae8214f54c9bbac52a
MohammedYazji <mahmadyazji@gmail.com>
2025-07-30 14:50:29 +0300

    implement another solution with match statement

--- my_code/DSA/PYTHON/DS/stack and queue/Evaluate_Reverse_Polish_Notation_(Postfix).py
+++ my_code/DSA/PYTHON/DS/stack and queue/Evaluate_Reverse_Polish_Notation_(Postfix).py
@@ -6,19 +6,36 @@ def evaluate_postfix(arr):
         for item in arr:
             # if the item is operator so pop the two numbers from the stack
             # then make the process and push the result instead of them
-            if item == '+':
-                stack.append(stack.pop() + stack.pop())
-            elif item == '-':
-                last, first = stack.pop(), stack.pop()
-                stack.append(first - last)
-            elif item == '*':
-                stack.append(stack.pop() * stack.pop())
-            elif item == '/':
-                last, first = stack.pop(), stack.pop()
-                stack.append(first // last)
-            # if is a number push it to the stack
-            else:
-                stack.append(int(item))
+            # if item == '+':
+            #     stack.append(stack.pop() + stack.pop())
+            # elif item == '-':
+            #     last, first = stack.pop(), stack.pop()
+            #     stack.append(first - last)
+            # elif item == '*':
+            #     stack.append(stack.pop() * stack.pop())
+            # elif item == '/':
+            #     last, first = stack.pop(), stack.pop()
+            #     stack.append(first // last)
+            # # if is a number push it to the stack
+            # else:
+            #     stack.append(int(item))
+
+            # DO IT WITH match [switch in other languages]
+            match item:
+                case '+':
+                    stack.append(stack.pop() + stack.pop())
+                case '-': 
+                    last, first = stack.pop(), stack.pop()
+                    stack.append(first - last)
+                case '*':
+                    stack.append(stack.pop() * stack.pop())
+                case '/':
+                    last, first = stack.pop(), stack.pop()
+                    stack.append(first // last)
+                # default case else
+                case _:
+                    stack.append(int(item))
+                
         # because now we have just one value in the stack
         return stack[0]
 
