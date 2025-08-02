95998e5e424b9093c33d0f6f731e3b2b898361d7
MohammedYazji <mahmadyazji@gmail.com>
2025-08-02 12:10:11 +0300

    Importing Review and User Data

--- tutorials_code/nodejs/4-natours/dev-data/data/import-dev-data.js
+++ tutorials_code/nodejs/4-natours/dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 // define path of config.env
 dotenv.config({ path: '../../config.env' });
@@ -26,11 +28,17 @@ mongoose
 // READ JSON FILE
 // here must use dirname instead of . because . refer to home node folder
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8'),
+);
 
 // IMPORT DATA INTO DB
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -42,6 +50,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
   } catch (err) {
     console.log(err);
--- tutorials_code/nodejs/4-natours/server.js
+++ tutorials_code/nodejs/4-natours/server.js
@@ -22,11 +22,14 @@ const DB = process.env.DATABASE.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err);
+    process.exit(1);
   });
 
 const port = process.env.PORT || 3000;
