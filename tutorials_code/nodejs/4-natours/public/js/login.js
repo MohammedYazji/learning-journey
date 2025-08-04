fbfb05088927c13f398689d9b649a8871d50ab52
MohammedYazji <mahmadyazji@gmail.com>
2025-08-04 16:20:41 +0300

    Logging in Users with Our API - Part 3

--- tutorials_code/nodejs/4-natours/public/js/login.js
+++ tutorials_code/nodejs/4-natours/public/js/login.js
@@ -1,6 +1,8 @@
 /*eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
 
-const login = async (email, password) => {
+export const login = async (email, password) => {
   try {
     // result
     const res = await axios({
@@ -14,20 +16,12 @@ const login = async (email, password) => {
 
     // implement that when i make the login request and the response get with status success so the credentials are true so reload the page and redirect to home page
     if (res.data.status === 'success') {
-      alert('Logged in successfully');
+      showAlert('success', 'Logged in successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-// make event listener when the form submit
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
--- tutorials_code/nodejs/4-natours/public/js/mapbox.js
+++ tutorials_code/nodejs/4-natours/public/js/mapbox.js
@@ -1,53 +1,50 @@
 /* eslint-disable */
 
-console.log('Hello from the client side');
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoibW9oYW1tZWR5YXpqaSIsImEiOiJjbWR3d3ZrNGMxcXIwMmtzYWFpZTQ4aHhuIn0.AMNqkmRjpK_D46ql_c221Q';
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mohammedyazji/cmdwxiqqo007401sd8vbsbk44', // style URL
+    scrollZoom: false,
+    // center: [-118.113491, 34.111745], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    // zoom: 10, // starting zoom
+    // interactive: false,
+  });
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoibW9oYW1tZWR5YXpqaSIsImEiOiJjbWR3d3ZrNGMxcXIwMmtzYWFpZTQ4aHhuIn0.AMNqkmRjpK_D46ql_c221Q';
+  const bounds = new mapboxgl.LngLatBounds();
 
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/mohammedyazji/cmdwxiqqo007401sd8vbsbk44', // style URL
-  scrollZoom: false,
-  // center: [-118.113491, 34.111745], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-  // zoom: 10, // starting zoom
-  // interactive: false,
-});
+  locations.forEach((loc) => {
+    // add marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-const bounds = new mapboxgl.LngLatBounds();
+    // add marker to the map
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-locations.forEach((loc) => {
-  // add marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    // add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+      .addTo(map);
+    // extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  // add marker to the map
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
-
-  // add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
-    .addTo(map);
-  // extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-}); // make smooth animation to fit the bounds
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  }); // make smooth animation to fit the bounds
+};
