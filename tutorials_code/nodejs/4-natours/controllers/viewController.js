c5b8d632920c29f9ec1e7d06ef9935194db6ceac
MohammedYazji <mahmadyazji@gmail.com>
2025-08-04 13:32:56 +0300

    Building the Login Screen

--- tutorials_code/nodejs/4-natours/controllers/viewController.js
+++ tutorials_code/nodejs/4-natours/controllers/viewController.js
@@ -14,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the request tour (including reviews and guides)
 
   // we populate the guide before
@@ -30,3 +30,9 @@ exports.getTour = catchAsync(async (req, res) => {
     tour,
   });
 });
+
+exports.getLoginForm = (req, res) => {
+  res.status(200).render('login', {
+    title: 'Log into your account',
+  });
+};
