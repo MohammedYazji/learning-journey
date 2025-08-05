97ca68612a844972cfd3a18498b78685eb2f03b7
MohammedYazji <mahmadyazji@gmail.com>
2025-08-05 19:45:32 +0300

    Creating New Bookings on Checkout Success

--- tutorials_code/nodejs/4-natours/routes/viewRoutes.js
+++ tutorials_code/nodejs/4-natours/routes/viewRoutes.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const viewController = require('../controllers/viewController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
 // use isLoggedIn Middleware which is similar a little bit to protect to decide how to render pages based on logged in or not
 // so this will apply all routes after this
 
-router.get('/', authController.isLoggedIn, viewController.getOverview);
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewController.getOverview,
+);
+
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
