8cd8010d5f6d9140a6c7d1ec9642897c98d4a332
MohammedYazji <mahmadyazji@gmail.com>
2025-08-05 15:55:41 +0300

    Using Brevo for Real Emails

--- tutorials_code/nodejs/4-natours/utils/email.js
+++ tutorials_code/nodejs/4-natours/utils/email.js
@@ -1,69 +1,70 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const htmlToText = require('html-to-text');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { htmlToText } = require('html-to-text');
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Mohammed Yazji <${process.env.EMAIL_FROM}>`;
+    this.from = `Seb <${process.env.EMAIL_FROM}>`;
   }
-
+  // eslint-disable-next-line lines-between-class-members
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       return nodemailer.createTransport({
-        service: 'SendGrid',
+        // service: 'Brevo'
+        host: process.env.BREVO_EMAIL_HOST,
+        port: process.env.BREVO_EMAIL_PORT,
         auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
+          user: process.env.BREVO_USERNAME,
+          pass: process.env.BREVO_PASSWORD,
         },
       });
     }
-    // 1) Create a test account
+
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: 587,
-      secure: false, // true for 465, false for other ports
+      host: process.env.BREVO_EMAIL_HOST,
+      port: process.env.BREVO_EMAIL_PORT,
       auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
+        user: process.env.BREVO_USERNAME,
+        pass: process.env.BREVO_PASSWORD,
       },
     });
   }
-
-  // 2) Send the actual email
+  // Send the actual email
+  // eslint-disable-next-line lines-between-class-members
   async send(template, subject) {
-    // 1) Render HTML based on a pug template
+    //1) Render HTML based on a pug template
     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
       firstName: this.firstName,
       url: this.url,
       subject,
     });
-
-    // 2) Define email options
+    //2) Define email options
     const mailOptions = {
       from: this.from,
       to: this.to,
-      subject,
-      html,
-      text: htmlToText.convert(html),
+      subject: subject,
+      html: html,
+      text: htmlToText(html),
+      //html:
     };
 
-    //3) Create a transport and send email
+    // 3) Create a transport and send email
     await this.newTransport().sendMail(mailOptions);
   }
 
-  // send welcome email
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
   }
 
-  // send password reset email
   async sendPasswordReset() {
     await this.send(
       'passwordReset',
-      'Your password reset token (valid for only 10 min)',
+      'Your password reset token (valid for only 10 minutes).',
     );
   }
 };
